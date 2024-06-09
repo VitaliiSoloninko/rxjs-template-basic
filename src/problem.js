60c0ad2571f9e519cc0b85011a9f123f90f4b3ae
22051988Di <vitalii.soloninko@gmail.com>
2024-06-09 18:48:37 +0200

    v0.0.1	Project start npm i, npm start

---
+++ src/problem.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById('interval')
+const rxjsBtn = document.getElementById('rxjs')
+const display = document.querySelector('#problem .result')
+
+const people = [
+	{ name: 'Vitalii', age: 37 },
+	{ name: 'Dilya', age: 36 },
+	{ name: 'Jasmina', age: 11 },
+	{ name: 'Lola', age: 8 },
+	{ name: 'Alsu', age: 6 },
+	{ name: 'Galina', age: 68 },
+	{ name: 'Tanya', age: 70 },
+]
