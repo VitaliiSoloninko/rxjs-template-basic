ecf2a2b33b8980f76c1bfaea7256d93719b0eb39
22051988Di <vitalii.soloninko@gmail.com>
2024-06-21 17:25:46 +0200

    v0.0.8	Draw program - From Event, click, scroll, mousemove

--- src/creation.js
+++ src/creation.js
@@ -1,7 +1,8 @@
-import { Observable } from 'rxjs'
 // of - creation streams from any data
 // of - work with numbers and string
 // from - work with array
+
+import { fromEvent, map } from 'rxjs'
 
 // const stream$ = of('Hello', 'World')
 // stream$.subscribe(val => {
@@ -29,20 +30,18 @@ import { Observable } from 'rxjs'
 // stream$.subscribe(val => console.log('Val:', val))
 
 // =================== Observable own new stream
+// const stream$ = new Observable(observer => {
+// 	observer.next('First value')
+// 	setTimeout(() => observer.next('After 1000 ms'), 1000)
 
-const stream$ = new Observable(observer => {
-	observer.next('First value')
-	setTimeout(() => observer.next('After 1000 ms'), 1000)
+// 	setTimeout(() => observer.complete(), 2000)
 
-	setTimeout(() => observer.complete(), 2000)
+// 	setTimeout(() => observer.error('Something went wrong'), 3000)
 
-	setTimeout(() => observer.error('Something went wrong'), 3000)
-
-	setTimeout(() => observer.next('After 4000 ms'), 4000)
-})
+// 	setTimeout(() => observer.next('After 4000 ms'), 4000)
+// })
 
 // =================== Observable subscribe to stream
-
 // ---------- 1 variant
 // stream$.subscribe(
 // 	val => console.log('Val:', val), // function val work with method next
@@ -51,14 +50,39 @@ const stream$ = new Observable(observer => {
 // )
 
 // ---------- 2 variant
-stream$.subscribe({
-	next(val) {
-		console.log(val)
-	},
-	error(err) {
-		console.log(err)
-	},
-	complete() {
-		console.log('Complete')
-	},
+// stream$.subscribe({
+// 	next(val) {
+// 		console.log(val)
+// 	},
+// 	error(err) {
+// 		console.log(err)
+// 	},
+// 	complete() {
+// 		console.log('Complete')
+// 	},
+// })
+
+// =================== fromEvent - basic
+// fromEvent(document.querySelector('canvas'), 'mousemove').subscribe(event =>
+// 	console.log(event)
+// )
+
+// =================== fromEvent - draw
+fromEvent(document.querySelector('canvas'), 'mousemove')
+	.pipe(
+		map(e => ({
+			x: e.offsetX,
+			y: e.offsetY,
+			ctx: e.target.getContext('2d'),
+		}))
+	)
+	.subscribe(pos => {
+		pos.ctx.fillRect(pos.x, pos.y, 2, 2)
+	})
+
+const clear$ = fromEvent(document.getElementById('clear'), 'click')
+
+clear$.subscribe(() => {
+	const canvas = document.querySelector('canvas')
+	canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
 })
