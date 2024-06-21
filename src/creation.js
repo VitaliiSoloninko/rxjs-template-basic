2a3d9d10559ee5bd48d88647bcdeb406a7a3ea5f
22051988Di <vitalii.soloninko@gmail.com>
2024-06-21 16:12:02 +0200

    v0.0.5	Import and create streems of and from

---
+++ src/creation.js
@@ -0,0 +1,15 @@
+// of - creation streams from any data
+// of - work with numbers and string
+// from - work with array
+
+// const stream$ = of('Hello', 'World')
+// stream$.subscribe(val => {
+// 	console.log('Value: ', val)
+// })
+
+// from - same as of but works with arrays
+// not work
+// const arr$ = from([1, 2, 3, 4]).pipe(scan((acc, v) => acc.concat(v), []))
+// arr$.subscribe(val => {
+// 	console.log(val)
+// })
